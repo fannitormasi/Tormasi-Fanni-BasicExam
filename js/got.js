c4001a5e7d18ebcba8ad8df95680be2fed0176e6
Fanni Tormasi <tormasifanni@gmail.com>
2018-12-09 18:11:24 +0100

    only search function is missing

--- js/got.js
+++ js/got.js
@@ -1,11 +1,11 @@
 function getGameOfThronesCharacterDatas(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       callbackFunc(this);
     }
   };
-  xhttp.open("GET", url, true);
+  xhttp.open('GET', url, true);
   xhttp.send();
 }
 
@@ -16,20 +16,23 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
 
   sortByName(userDatas);
   getPortraits(userDatas);
+  console.log(userDatas);
 
-  var clickedPortait = document.querySelectorAll(".portraits");
+  // visszadja a console-ra annak a karakternek a nevét, akire kattintottunk
+  var clickedPortait = document.querySelectorAll('.portraits');
   for (var i = 0; i < clickedPortait.length; i++) {
     if (clickedPortait.hasOwnProperty(i)) {
-      clickedPortait[i].addEventListener("click", function() {
-        var nameOfCharacter = this.querySelector(".names").innerHTML;
-        console.log(nameOfCharacter);
+      clickedPortait[i].addEventListener('click', function () {
+        var name = this.querySelector('.names').innerHTML;
+        whichCharacter(userDatas, name);
+        console.log(name);
       });
     }
   }
 }
 
 getGameOfThronesCharacterDatas(
-  "./json/got.json",
+  './json/got.json',
   successGetGameOfThronesCharacterDatas
 );
 /*
@@ -55,19 +58,74 @@ function sortByName(arrayOfCharacters) {
 
 // ha a karakter nem halott, vissza adja az adott karakter portréját és nevét
 function getPortraits(arrayOfCharacters) {
-  var tableRow = "";
+  var tableRow = '';
   for (var i = 0; i < arrayOfCharacters.length; i++) {
     if (arrayOfCharacters.hasOwnProperty(i) && !arrayOfCharacters[i].dead) {
       tableRow += `
             <div class="portraits">
-              <img class="pics" src="/${
-                arrayOfCharacters[i].portrait
-              }" alt=""><br>
-            <label class="names">${arrayOfCharacters[i].name}
-              </label>
+              <img class="pics" id="${i}" src="/${
+  arrayOfCharacters[i].portrait
+}" alt=""><br>
+            <label for="${i}" class="names">${arrayOfCharacters[i].name}</label>
               </div>
   `;
     }
   }
-  document.querySelector("#pics").innerHTML = tableRow;
+  document.querySelector('#pics').innerHTML = tableRow;
+}
+
+// a console-ra kiíratott nevet megkeresi a tömb elemei között és ha megtalálja, akkor lefuttatja a négy függvényt,
+// amik beillesztik a karakter képét, nevét, házát és leírását
+function whichCharacter(arrayOfCharacters, nameOfCharacter) {
+  for (var i = 0; i < arrayOfCharacters.length; i++) {
+    if (arrayOfCharacters[i].name === nameOfCharacter) {
+      if (arrayOfCharacters[i].picture) {
+        showPicture(arrayOfCharacters[i].picture, arrayOfCharacters[i].name);
+      } else {
+        showPicture('/assets/pictures/jorah.webp');
+      }
+      showName(arrayOfCharacters[i].name);
+      if (arrayOfCharacters[i].house) {
+        showHouse(arrayOfCharacters[i].house);
+        showDesc(arrayOfCharacters[i].bio);
+      }
+    }
+  }
+}
+
+function showPicture(picture) {
+  var whereToPutPicture = document.querySelector('#showPicture');
+  whereToPutPicture.innerHTML = '';
+  var image = document.createElement('img');
+  image.src = picture;
+  image.alt = '';
+  image.className = 'pictureOfCharacter';
+  whereToPutPicture.appendChild(image);
+}
+
+function showName(name) {
+  var whereToPutName = document.querySelector('#showName');
+  whereToPutName.innerHTML = '';
+  var paragraph = document.createElement('p');
+  paragraph.innerHTML = name;
+  paragraph.className = 'namesOfCharacter';
+  whereToPutName.appendChild(paragraph);
+}
+
+function showHouse(house) {
+  var whereToPutHouse = document.querySelector('#showHouse');
+  whereToPutHouse.innerHTML = '';
+  var imageHouse = document.createElement('img');
+  imageHouse.src = `/assets/houses/${house}.png`;
+  imageHouse.className = 'housesOfCharacter';
+  whereToPutHouse.appendChild(imageHouse);
+}
+
+function showDesc(desc) {
+  var whereToPutDesc = document.querySelector('#showDesc');
+  whereToPutDesc.innerHTML = '';
+  var paragraphDesc = document.createElement('p');
+  paragraphDesc.innerHTML = desc;
+  paragraphDesc.className = 'descOfCharacter';
+  whereToPutDesc.appendChild(paragraphDesc);
 }
